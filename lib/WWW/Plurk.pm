b80f506f9bd7771803f3a0f63653f3ea432c8986
Andy Armstrong <andy@hexten.net>
2008-06-15 17:56:33 +0000

    As released

--- lib/WWW/Plurk.pm
+++ lib/WWW/Plurk.pm
@@ -28,10 +28,19 @@ our $VERSION = '0.01';
 =head1 SYNOPSIS
 
     use WWW::Plurk;
-  
+    my $plurk = WWW::Plurk->new;
+    $plurk->login( 'username', 'password' );
+    my $msg = $plurk->add_plurk( content => 'Hello, World' );
+
 =head1 DESCRIPTION
 
-Based on Ryan Lim's unofficial PHP API: L<http://code.google.com/p/rlplurkapi/>
+This is an unofficial API for plurk.com. It uses the same interfaces
+that plurk itself uses internally which are not published and not
+necessarily stable. When Plurk publish a stable API this module will be
+updated to take advantage of it. In the mean time use with caution.
+
+Ryan Lim did the heavy lifting of reverse engineering the API. His PHP
+implementation can be found at L<http://code.google.com/p/rlplurkapi/>.
 
 =cut
 
@@ -58,7 +67,7 @@ my %PATH_DEFAULT = (
 
 BEGIN {
     my @ATTR = qw(
-      base_uri
+      _base_uri
       info
       state
     );
@@ -101,17 +110,40 @@ BEGIN {
 
 =head1 INTERFACE 
 
+All methods throw errors in the event of any kind of failure. There's no
+need to check return values but you might want to wrap calls in an
+C<eval> block.
+
 =head2 C<< new >>
+
+Create a new C<< WWW::Plurk >>. Optionally accepts two arguments
+(username, password). If they are supplied it will attempt to login to
+Plurk. If no arguments are supplied C<login> must be called before
+attempting to access the service.
+
+    # Create and login
+    my $plurk = WWW::Plurk->new( 'user', 'pass' );
+    
+    # Create then login afterwards
+    my $plurk = WWW::Plurk->new;
+    $plurk->login( 'user', 'pass' );
 
 =cut
 
 sub new {
     my $class = shift;
     my $self  = bless {
-        base_uri => $BASE_DEFAULT,
-        path     => {%PATH_DEFAULT},
-        state    => 'init',
+        _base_uri => $BASE_DEFAULT,
+        path      => {%PATH_DEFAULT},
+        state     => 'init',
     }, $class;
+
+    if ( @_ ) {
+        croak "Need two arguments (user, pass) if any are supplied"
+          unless @_ == 2;
+        $self->login( @_ );
+    }
+
     return $self;
 }
 
@@ -133,7 +165,7 @@ sub _cookies { shift->_ua->cookie_jar }
 sub _post {
     my ( $self, $service, $params ) = @_;
     my $resp
-      = $self->_ua->post( $self->uri_for( $service ), $params || {} );
+      = $self->_ua->post( $self->_uri_for( $service ), $params || {} );
     croak $resp->status_line
       unless $resp->is_success
           or $resp->is_redirect;
@@ -148,7 +180,7 @@ sub _json_post {
 sub _get {
     my ( $self, $service, $params ) = @_;
     my $resp
-      = $self->_ua->get( $self->uri_for( $service, $params || {} ) );
+      = $self->_ua->get( $self->_uri_for( $service, $params || {} ) );
     croak $resp->status_line
       unless $resp->is_success
           or $resp->is_redirect;
@@ -161,6 +193,12 @@ sub _json_get {
 }
 
 =head2 C<< login >>
+
+Attempt to login to a Plurk account. The two mandatory arguments are the
+username and password for the account to be accessed.
+
+    my $plurk = WWW::Plurk->new;
+    $plurk->login( 'user', 'pass' );
 
 =cut
 
@@ -179,7 +217,7 @@ sub login {
     croak "Login for $name failed, no cookie returned"
       unless $ok;
 
-    $self->path_for( home => $resp->header( 'Location' )
+    $self->_path_for( home => $resp->header( 'Location' )
           || "/user/$name" );
 
     $self->_parse_user_home;
@@ -190,6 +228,26 @@ sub _parse_time {
     my ( $self, $time ) = @_;
     return DateTime::Format::Mail->parse_datetime( $time )->epoch;
 }
+
+# This is a bit of a bodge. Plurk doesn't return pure JSON; instead it
+# returns JavaScript that's nearly JSON apart from the fact that
+# timestamps are specified as 'new Date("...")'. So we need to hoist
+# those out of the text and replace them with the corresponding epoch
+# timestamp.
+#
+# Theoretically we could just do a search and replace. Because the Date
+# constructor contains a quoted string there's no danger of false
+# positives when someone happens to post a message that contains
+# matching text - because in that case the nested quotes would be
+# backslashed and the regex wouldn't match.
+#
+# Of course that didn't occur to me until /after/ I'd written the code
+# to pull all the string literals out of the text before replacing the
+# Date constructors...
+#
+# I'll leave that code in place because it's useful to have lying around
+# in case some future version of this routine has to handle embedded JS
+# that could collide with the contents of string literals.
 
 sub _decode_json {
     my ( $self, $json ) = @_;
@@ -214,6 +272,7 @@ sub _decode_json {
     # Replace string literals
     $json =~ s{ " (\#\d+\#) " }{ qq{"$strings{$1}"} }xeg;
 
+    # Now we have JSON
     return decode_json $json;
 }
 
@@ -234,6 +293,12 @@ sub _parse_user_home {
 
 =head2 C<< is_logged_in >>
 
+Returns a true value if we're currently logged in.
+
+    if ( $plurk->is_logged_in ) {
+        $plurk->add_plurk( content => 'w00t!' );
+    }
+
 =cut
 
 sub is_logged_in { shift->state eq 'login' }
@@ -245,6 +310,27 @@ sub _logged_in {
 }
 
 =head2 C<< friends_for >>
+
+Return a user's friends.
+
+    my @friends = $plurk->friends_for( $uid );
+
+Pass the user id as either
+
+=over
+
+=item * an integer
+
+    my @friends = $plurk->friends_for( 12345 );
+
+=item * an object that has a method called C<uid>
+
+    # $some_user isa WWW::Plurk::Friend
+    my @friends = $plurk->friends_for( $some_user );
+
+=back
+
+Returns a list of L<WWW::Plurk::Friend> objects.
 
 =cut
 
@@ -259,6 +345,14 @@ sub friends_for {
 }
 
 =head2 C<< friends >>
+
+Return the current user's friends. This
+
+    my @friends = $plurk->friends;
+
+is equivalent to
+
+    my @friends = $plurk->friends_for( $self->uid );
 
 =cut
 
@@ -275,6 +369,53 @@ Post a new plurk.
         content => 'Hello, World'
     );
 
+Arguments are supplied as a number of key, value pairs. The following
+arguments are recognised:
+
+=over
+
+=item * content - the message content
+
+=item * qualifier - the qualifier string ('is', 'says' etc)
+
+=item * lang - the (human) language for this Plurk
+
+=item * no_comments - true to disallow comments
+
+=item * limited_to - limit visibility
+
+=back
+
+The only mandatory argument is C<content> which should be a string of
+140 characters or fewer.
+
+C<qualifier> is first word of the message - which has special
+significance that you will understand if you have looked at the Plurk
+web interface. The following qualifiers are supported:
+
+  asks feels gives has hates is likes loves 
+  says shares thinks wants was will wishes
+
+If omitted C<qualifier> defaults to ':' which signifies that you are
+posting a free-form message with no qualifier.
+
+C<lang> is the human language for this Plurk. It defaults to 'en'.
+Apologies to those posting in languages other than English.
+
+C<no_comments> should be true to lock the Plurk preventing comments from
+being made.
+
+C<limited_to> is an array of user ids (or objects with a method called
+C<uid>). If present the Plurk will only be visible to those users. To
+limit visibility of a Plurk to friends use:
+
+    my $msg = $plurk->add_plurk(
+        content => 'Hi chums',
+        limited_to => [ $plurk->friends ]
+    );
+
+Returns a L<WWW::Plurk::Message> representing the new Plurk.
+
 =cut
 
 sub _is_user {
@@ -287,21 +428,21 @@ sub _uid_cast {
     return $self->_is_user( $obj ) ? $obj->uid : $obj;
 }
 
-sub add_plurk {
-    my ( $self, @args ) = @_;
+sub _msg_common {
+    my ( $self, $cb, @args ) = @_;
+
     croak "Needs a number of key => value pairs"
       if @args & 1;
     my %args = @args;
 
     my $content = delete $args{content} || croak "Must have content";
     my $lang    = delete $args{lang}    || 'en';
-    my $qualifier = delete $args{qualifier} || 'says';
-    my $no_comments = delete $args{no_comments};
+    my $qualifier = delete $args{qualifier} || ':';
 
-    my @limit = @{ delete $args{limit} || [] };
+    my @extras = $cb->( \%args );
 
-    if ( my @extra = sort keys %args ) {
-        croak "Unknown parameter(s): ", join ',', @extra;
+    if ( my @unknown = sort keys %args ) {
+        croak "Unknown parameter(s): ", join ',', @unknown;
     }
 
     if ( length $content > MAX_MESSAGE_LENGTH ) {
@@ -309,6 +450,23 @@ sub add_plurk {
           . MAX_MESSAGE_LENGTH
           . ' characters';
     }
+
+    return ( $content, $lang, $qualifier, @extras );
+}
+
+sub add_plurk {
+    my ( $self, @args ) = @_;
+
+    my ( $content, $lang, $qualifier, $no_comments, @limit )
+      = $self->_msg_common(
+        sub {
+            my $args        = shift;
+            my $no_comments = delete $args->{no_comments};
+            my @limit       = @{ delete $args->{limit} || [] };
+            return ( $no_comments, @limit );
+        },
+        @args
+      );
 
     my $reply = $self->_json_get(
         add_plurk => {
@@ -329,15 +487,37 @@ sub add_plurk {
         croak "Error posting: $error";
     }
 
-    return WWW::Plurk::Message->new( $reply->{plurk} );
-
+    return WWW::Plurk::Message->new( $self, $reply->{plurk} );
 }
 
-=head2 C<< get_plurks >>
+=head2 C<< plurks >>
+
+Get a list of recent Plurks for the logged in user. Returns an array of
+L<WWW::Plurk::Message> objects.
+
+    my @plurks = $plurk->plurks;
+
+Any arguments must be passed as key => value pairs. The following
+optional arguments are recognised:
+
+=over
+
+=item * uid - the user whose messages we want
+
+=item * date_from - the start date for retrieved messages
+
+=item * date_offset - er, not sure what this does :)
+
+=back
+
+As you may infer from the explanation of C<date_offset>, I'm not
+entirely sure how this interface works. I cargo-culted the options from
+the PHP version. If anyone can explain C<date_offset> please let me know
+and I'll update the documentation.
 
 =cut
 
-sub get_plurks {
+sub plurks {
     my ( $self, @args ) = @_;
     croak "Needs a number of key => value pairs"
       if @args & 1;
@@ -345,7 +525,6 @@ sub get_plurks {
 
     my $uid = $self->_uid_cast( delete $args{uid} || $self );
 
-    my $responses   = delete $args{responses};
     my $date_from   = delete $args{date_from};
     my $date_offset = delete $args{date_offset};
 
@@ -365,18 +544,60 @@ sub get_plurks {
         }
     );
 
-    return map { WWW::Plurk::Message->new( $self, $_ ) } @{$reply};
+    return
+      map { WWW::Plurk::Message->new( $self, $_ ) } @{ $reply || [] };
 }
 
-=head2 C<< get_responses_for >>
+=head2 C<< unread_plurks >>
+
+Return a list of unread Plurks for the current user.
 
 =cut
 
-sub get_responses_for {
+sub unread_plurks {
+    my $self = shift;
+    my $reply = $self->_json_post( get_unread_plurks => {} );
+    return
+      map { WWW::Plurk::Message->new( $self, $_ ) } @{ $reply || [] };
+}
+
+# Plurk returns an empty array rather than an empty hash if there
+# are no elements. D'you think it's written in PHP? :)
+#
+# (That's not a dig at PHP, but since arrays and hashes are the same
+# thing in PHP I assume the JSON encoder can't tell what an empty
+# hash/array is)
+
+sub _want_hash {
+    my ( $self, $hash, @keys ) = @_;
+    for my $key ( @keys ) {
+        if (
+            !exists $hash->{$key}
+            || ( 'ARRAY' eq ref $hash->{$key}
+                && @{ $hash->{$key} } == 0 )
+          ) {
+            $hash->{$key} = {};
+        }
+    }
+}
+
+=head2 C<< responses_for >>
+
+Get the responses for a Plurk. Returns a list of
+L<WWW::Plurk::Message> objects. Accepts a single argument which is the
+numeric ID of the Plurk whose responses we want.
+
+    my @responses = $plurk->responses_for( $msg->plurk_id );
+
+=cut
+
+sub responses_for {
     my ( $self, $plurk_id ) = @_;
 
     my $reply
       = $self->_json_post( get_responses => { plurk_id => $plurk_id } );
+
+    $self->_want_hash( $reply, 'friends' );
 
     my %friends = map {
         $_ =>
@@ -385,14 +606,62 @@ sub get_responses_for {
 
     return map {
         WWW::Plurk::Message->new( $self, $_, $friends{ $_->{user_id} } )
-    } @{ $reply->{responses} };
+    } @{ $reply->{responses} || [] };
 }
 
-=head2 C<< path_for >>
+=head2 C<< respond_to_plurk >>
+
+Post a response to an existing Plurk. The first argument must be the ID
+of the Plurk to respond to. Additional arguments are supplied as a
+number of key => value pairs. The following arguments are recognised:
+
+=over
+
+=item * content - the message content
+
+=item * qualifier - the qualifier string ('is', 'says' etc)
+
+=item * lang - the (human) language for this Plurk
+
+=back
+
+See C<add_plurk> for details of how these arguments are interpreted.
+
+    my $responce = $plurk->respond_to_plurk(
+        $plurk_id,
+        content => 'Nice!'
+    );
+
+Returns an L<WWW::Plurk::Message> representing the newly posted
+response.
 
 =cut
 
-sub path_for {
+sub respond_to_plurk {
+    my ( $self, $plurk_id, @args ) = @_;
+
+    my ( $content, $lang, $qualifier )
+      = $self->_msg_common( sub { () }, @args );
+
+    my $reply = $self->_json_get(
+        add_response => {
+            posted    => localtime()->datetime,
+            qualifier => $qualifier,
+            content   => $content,
+            lang      => $lang,
+            p_uid     => $self->uid,
+            plurk_id  => $plurk_id,
+        }
+    );
+
+    if ( my $error = $reply->{error} ) {
+        croak "Error posting: $error";
+    }
+
+    return WWW::Plurk::Message->new( $self, $reply->{object} );
+}
+
+sub _path_for {
     my ( $self, $service ) = ( shift, shift );
     croak "Unknown service $service"
       unless exists $PATH_DEFAULT{$service};
@@ -400,51 +669,51 @@ sub path_for {
     return $self->{path}{$service} = shift;
 }
 
-=head2 C<< uri_for >>
-
-Return the uri for part of the service
-
-=cut
-
-sub uri_for {
+sub _uri_for {
     my ( $self, $service ) = ( shift, shift );
-    my $uri = $self->base_uri . $self->path_for( $service );
+    my $uri = $self->_base_uri . $self->_path_for( $service );
     return $uri unless @_;
     my $params = shift;
     return join '?', $uri, HTML::Tiny->new->query_encode( $params );
 }
 
-=head2 C<< base_uri >>
+=head2 Accessors
 
-=head2 C<< info >>
+The following accessors are available:
 
-=head2 C<< state >>
+=over
 
-=head2 C<< display_name >>
+=item * C<< info >> - the user info hash
 
-=head2 C<< full_name >>
+=item * C<< state >> - the state of this object (init or login)
 
-=head2 C<< gender >>
+=item * C<< display_name >> - the user's display name
 
-=head2 C<< has_profile_image >>
+=item * C<< full_name >> - the user's full name
 
-=head2 C<< id >>
+=item * C<< gender >> - the user's gender
 
-=head2 C<< is_channel >>
+=item * C<< has_profile_image >> - has a profile image?
 
-=head2 C<< karma >>
+=item * C<< id >> - appears to be a synonym for uid
 
-=head2 C<< location >>
+=item * C<< is_channel >> - unknown; anyone know?
 
-=head2 C<< nick_name >>
+=item * C<< karma >> - user's karma score
 
-=head2 C<< page_title >>
+=item * C<< location >> - user's location
 
-=head2 C<< relationship >>
+=item * C<< nick_name >> - user's nick name
 
-=head2 C<< star_reward >>
+=item * C<< page_title >> - unknown; anyone know?
 
-=head2 C<< uid >>
+=item * C<< relationship >> - married, single, etc
+
+=item * C<< star_reward >> - ???
+
+=item * C<< uid >> - the user's ID
+
+=back
 
 =cut
 
